d6ea3028fce61b17266e2a04e66b48efad2c4786
BriCortesTec <23160988@itoaxaca.edu.mx>
2026-06-02 04:47:43 -0600

    Coordi casa terminada

---
+++ completar_reporte.php
@@ -0,0 +1,28 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
+include("conexion.php");
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+$id = $data["id_reporte"];
+
+$query = "
+
+UPDATE tuto.reportes
+
+SET estatus = 'Completado'
+
+WHERE id_reporte = $id
+
+";
+
+$resultado = pg_query($conexion, $query);
+
+echo json_encode([
+  "success" => $resultado
+]);
+
+?>
